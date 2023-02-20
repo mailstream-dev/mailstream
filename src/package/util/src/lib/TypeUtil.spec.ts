d83a1149f0c24717375af14392258e6d2002c954
Zachary <zatipnis@icloud.com>
2023-02-20 12:53:15 -0500

    more build-updates

--- src/package/util/src/lib/TypeUtil.spec.ts
+++ src/package/util/src/lib/TypeUtil.spec.ts
@@ -3,18 +3,18 @@ import { isNonEmptyString } from "./TypeUtil";
 describe("TypeUtil", () => {
   describe(isNonEmptyString, () => {
     test("Detects Strings", () => {
-      expect(isNonEmptyString("Hello World")).toBe(true);
-      expect(isNonEmptyString("")).toBe(false);
+      expect(isNonEmptyString("Hello World")).toBeTruthy();
+      expect(isNonEmptyString("")).toBeFalsy();
     });
 
     test("Doesn't pass for arrays", () => {
-      expect(isNonEmptyString(["a", "s", "d", "f"])).toBe(false);
-      expect(isNonEmptyString([1, 2, 3, 4])).toBe(false);
+      expect(isNonEmptyString(["a", "s", "d", "f"])).toBeFalsy();
+      expect(isNonEmptyString([1, 2, 3, 4])).toBeFalsy();
     });
 
     test("Doesn't pass for undefined or null", () => {
-      expect(isNonEmptyString(null)).toBe(false);
-      expect(isNonEmptyString(undefined)).toBe(false);
+      expect(isNonEmptyString(null)).toBeFalsy();
+      expect(isNonEmptyString(undefined)).toBeFalsy();
     });
   });
 });
