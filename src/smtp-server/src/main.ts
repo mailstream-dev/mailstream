9dee30a6e342f144f380e7808d461ef3e444952b
Zachary <zatipnis@icloud.com>
2023-02-28 16:19:34 -0500

    revert test change

--- src/smtp-server/src/main.ts
+++ src/smtp-server/src/main.ts
@@ -1,3 +1,18 @@
-import config from "@mailstream/config";
+import { SMTPServer } from "@mailstream/smtp";
 
-console.log(config.smtp);
+const options = {
+  // key: fs.readFileSync(
+  //   path.resolve(__dirname, "../../../local-assets/server-key.pem")
+  // ),
+  // cert: fs.readFileSync(
+  //   path.resolve(__dirname, "../../../local-assets/server-cert.pem")
+  // ),
+  // ca: fs.readFileSync(
+  //   path.resolve(__dirname, "../../../local-assets/server-csr.pem")
+  // ),
+  port: 8025,
+};
+
+new SMTPServer(console.log, options).listen(() => {
+  console.log(`Listening on ${options.port}`);
+});
